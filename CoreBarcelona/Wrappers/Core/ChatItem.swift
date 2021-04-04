2764f40aad0bc576347570c319e644e95911bb67
Eric Rabil <ericjrabil@gmail.com>
2021-04-04 18:21:27 -0400

    Added convenience methods for inspecting item ID/sender without switch/casing it

--- CoreBarcelona/Wrappers/Core/ChatItem.swift
+++ CoreBarcelona/Wrappers/Core/ChatItem.swift
@@ -35,6 +35,88 @@ public indirect enum ChatItem {
     case sticker(_ item: StickerChatItem)
     case action(_ item: ActionChatItem)
     
+    public var id: String? {
+        switch self {
+        case .date(let item):
+            return item.id
+        case .sender(let item):
+            return item.id
+        case .participantChange(let item):
+            return item.id
+        case .attachment(let item):
+            return item.id
+        case .status(let item):
+            return item.id
+        case .groupAction(let item):
+            return item.id
+        case .plugin(let item):
+            return item.id
+        case .text(let item):
+            return item.id
+        case .acknowledgment(let item):
+            return item.id
+        case .associated(let item):
+            return item.id
+        case .message(let item):
+            return item.id
+        case .phantom(let item):
+            return item.id
+        case .groupTitle(let item):
+            return item.id
+        case .typing(let item):
+            return item.id
+        case .sticker(let item):
+            return item.id
+        case .action(let item):
+            return item.id
+        }
+    }
+    
+    public var sender: String? {
+        get {
+            switch self {
+            case .groupAction(let item):
+                return item.sender
+            case .acknowledgment(let item):
+                return item.sender
+            case .message(let item):
+                return item.sender
+            case .groupTitle(let item):
+                return item.sender
+            case .typing(let item):
+                return item.sender
+            case .action(let item):
+                return item.sender
+            default:
+                return nil
+            }
+        }
+        set {
+            switch self {
+            case .groupAction(var item):
+                item.sender = newValue
+                self = .groupAction(item)
+            case .acknowledgment(var item):
+                item.sender = newValue
+                self = .acknowledgment(item)
+            case .message(var item):
+                item.sender = newValue
+                self = .message(item)
+            case .groupTitle(var item):
+                item.sender = newValue
+                self = .groupTitle(item)
+            case .typing(var item):
+                item.sender = newValue
+                self = .typing(item)
+            case .action(var item):
+                item.sender = newValue
+                self = .action(item)
+            default:
+                return
+            }
+        }
+    }
+    
     public var messageValue: Message? {
         guard case .message(let message) = self else {
             return nil
