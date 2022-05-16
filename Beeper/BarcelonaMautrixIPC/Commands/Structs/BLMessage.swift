b63e70cfa2ba3d0bc0d2755295878fcbfddd8d36
Eric Rabil <ericjrabil@gmail.com>
2022-05-16 11:01:14 -0400

    Revert "Revert iMessage-only GUID until mautrix-imessage support lands"
    This reverts commit 9a45718cf6a4dd8bcc00d78ce367aa2c17a6c72f.

--- Beeper/BarcelonaMautrixIPC/Commands/Structs/BLMessage.swift
+++ Beeper/BarcelonaMautrixIPC/Commands/Structs/BLMessage.swift
@@ -18,7 +18,7 @@ internal extension Chat {
 
 internal extension IMChat {
     var blChatGUID: String {
-        "\(account.serviceName ?? "SMS");\(isGroup ? "+" : "-");\(id)"
+        "iMessage;\(isGroup ? "+" : "-");\(id)"
     }
 }
 
