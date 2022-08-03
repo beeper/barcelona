208aff92d7b96faa6bd5c2b593782c5ce039e0c8
Eric Rabil <ericjrabil@gmail.com>
2022-08-03 14:03:15 -0400

    Expose fastDropURIPrefix in non-scratchbox builds

---
+++ Core/Barcelona/Extensions/Foundation/String+FastDropURI.swift
@@ -0,0 +1,20 @@
+//
+//  String+FastDropURI.swift
+//  Barcelona
+//
+//  Created by Eric Rabil on 8/3/22.
+//
+
+import Foundation
+
+extension String {
+    var fastDroppingURIPrefix: String {
+        guard let index = firstIndex(of: ":") else {
+            return self
+        }
+        guard index != endIndex else {
+            return ""
+        }
+        return String(self[self.index(after: index)...])
+    }
+}
