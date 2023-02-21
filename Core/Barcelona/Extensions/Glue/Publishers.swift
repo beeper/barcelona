5466359bfbd58053d82a717189c0e43ac44faf2e
Joonas Myhrberg <joonas@myhrberg.email>
2023-02-21 19:33:35 +0200

    Remove Publisher.toFuture extension

--- Core/Barcelona/Extensions/Glue/Publishers.swift
+++ Core/Barcelona/Extensions/Glue/Publishers.swift
@@ -9,18 +9,6 @@ import Foundation
 import Combine
 
 extension Publisher {
-    func toFuture() -> Future<Output, Failure> {
-        return Future<Output, Failure> { resolve in
-            self.retainingSink {
-                if case let .failure(error) = $0 {
-                    resolve(.failure(error))
-                }
-            } receiveValue: {
-                resolve(.success($0))
-            }
-        }
-    }
-
     @discardableResult
     func retainingSink(receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void, receiveValue: @escaping (Output) -> Void) -> AnyCancellable? {
         var cancellable: AnyCancellable?
