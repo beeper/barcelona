4ac96112c0d29b4c7fa3057314378754c6916772
Joonas Myhrberg <joonas@myhrberg.email>
2023-04-06 21:40:42 +0300

    Delete unused Publishers extension

--- Core/Barcelona/Extensions/Glue/Publishers.swift
+++ Core/Barcelona/Extensions/Glue/Publishers.swift
@@ -8,26 +8,6 @@
 import Combine
 import Foundation
 
-extension Publisher {
-    @discardableResult
-    func retainingSink(
-        receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void,
-        receiveValue: @escaping (Output) -> Void
-    ) -> AnyCancellable? {
-        var cancellable: AnyCancellable?
-        cancellable = sink(
-            receiveCompletion: {
-                receiveCompletion($0)
-
-                withExtendedLifetime(cancellable) { cancellable = nil }
-            },
-            receiveValue: receiveValue
-        )
-
-        return cancellable
-    }
-}
-
 extension Sequence {
     func asyncMap<T>(_ transform: (Element) async throws -> T) async rethrows -> [T] {
         var values = [T]()
