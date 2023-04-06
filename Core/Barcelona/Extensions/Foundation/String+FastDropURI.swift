750f204ddeeac4cbe029e955db6e67cc152a0391
Joonas Myhrberg <joonas@myhrberg.email>
2023-04-06 21:38:14 +0300

    Delete unused Foundation extensions

--- Core/Barcelona/Extensions/Foundation/String+FastDropURI.swift
+++ Core/Barcelona/Extensions/Foundation/String+FastDropURI.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-extension String {
-    var fastDroppingURIPrefix: String {
-        guard let index = firstIndex(of: ":") else {
-            return self
-        }
-        guard index != endIndex else {
-            return ""
-        }
-        return String(self[self.index(after: index)...])
-    }
-}
-
 extension CustomDebugStringConvertible {
     var singleLineDebugDescription: String {
         debugDescription.replacingOccurrences(of: "\n", with: " ")
