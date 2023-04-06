ed83c4b94ca4aa1deb48e712a68a2f6c7e22af52
Joonas Myhrberg <joonas@myhrberg.email>
2023-04-06 21:31:45 +0300

    Delete unused code from MessageAttributes

--- Core/Barcelona/Enums/MessageAttributes.swift
+++ Core/Barcelona/Enums/MessageAttributes.swift
@@ -18,23 +18,14 @@ struct MessageAttributes {
     static let transferGUID = NSAttributedString.Key(rawValue: IMFileTransferGUIDAttributeName)
     static let messagePart = NSAttributedString.Key(rawValue: IMMessagePartAttributeName)
     static let filename = NSAttributedString.Key(rawValue: IMFilenameAttributeName)
-    static let dataDetected = NSAttributedString.Key(rawValue: IMDataDetectedAttributeName)
-    static let noRichLink = NSAttributedString.Key(rawValue: "__kIMNoRichLinkAttributeName")
     static let bold = NSAttributedString.Key(rawValue: IMBoldAttributeName)
     static let italic = NSAttributedString.Key(rawValue: IMItalicAttributeName)
     static let underline = NSAttributedString.Key(rawValue: IMUnderlineAttributeName)
     static let strike = NSAttributedString.Key(rawValue: IMStrikethroughAttributeName)
-    static let fontSize = NSAttributedString.Key(rawValue: IMFontSizeAttributeName)
     static let calendarData = NSAttributedString.Key(rawValue: IMCalendarEventAttributeName)
     static let breadcrumbOptions = NSAttributedString.Key(rawValue: IMBreadcrumbTextOptionFlags)
     static let breadcrumbMarker = NSAttributedString.Key(rawValue: IMBreadcrumbTextMarkerAttributeName)
 
     @available(macOS 10.16, *)
     static let mentionName = NSAttributedString.Key(rawValue: IMMentionConfirmedMention)
-}
-
-public enum IMBaseWritingDirection: NSInteger {
-    case natural = -1
-    case lefttoRight = 0
-    case rightToLeft = 1
 }
