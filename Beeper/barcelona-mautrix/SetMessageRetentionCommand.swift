9a60a4f1b7e341c1e1a6402ca058962c979d8da9
itsjunetime <junewelker@gmail.com>
2023-08-02 23:01:19 -0600

    More accurately check stored value (I think), avoid usage of DarwinBoolean, and check return value of CFPreferencesSynchronize

--- Beeper/barcelona-mautrix/SetMessageRetentionCommand.swift
+++ Beeper/barcelona-mautrix/SetMessageRetentionCommand.swift
@@ -18,26 +18,26 @@ class SetMessageRetentionCommand: Command {
     @Param var days: Int
 
     func execute() throws {
+        log.info("Got request to set retention to \(days) day(s)")
+
         let mobileSMS = "com.apple.MobileSMS" as CFString
         let key = "KeepMessageForDays" as CFString
-        var currentVal: DarwinBoolean = false
-        withUnsafeMutablePointer(to: &currentVal) { hasVal in
-            // Check if we've already set the value to 1
-            let val = CFPreferencesGetAppIntegerValue(key, mobileSMS, hasVal)
-
-            // if we have already set the value correctly, obviously don't do anything
-            if hasVal.pointee != true || val != days {
-                log.info("\(key) was not already set, changing to \(days)")
-                // If it's not set, just set it for the current user and host
-                CFPreferencesSetValue(key, days as CFNumber, mobileSMS, kCFPreferencesCurrentUser, kCFPreferencesAnyHost)
-                CFPreferencesAppSynchronize(mobileSMS)
+        // Check if we've already set the value to days
+        if (CFPreferencesCopyValue(key, mobileSMS, kCFPreferencesCurrentUser, kCFPreferencesAnyHost) as? CFIndex) != days {
+            log.info("\(key) was not already set, changing to \(days)")
+            // If it's not set, just set it for the current user and host
+            CFPreferencesSetValue(key, days as CFNumber, mobileSMS, kCFPreferencesCurrentUser, kCFPreferencesAnyHost)
+            if CFPreferencesSynchronize(mobileSMS, kCFPreferencesCurrentUser, kCFPreferencesAnyHost) {
                 // the IMAutomaticHistoryDeletionAgent is only launched when manually invoked or this notification is posted, so this should activate it
                 notify_post("com.apple.imautomatichistorydeletionagent.prefchange")
 
                 log.info("Set preferences value and notified deletion agent!")
             } else {
-                log.info("\(key) was already set to \(days); not doing anything")
+                log.warning("Couldn't synchronize preferences for \(mobileSMS)")
             }
+        } else {
+            // if we have already set the value correctly, obviously don't do anything
+            log.info("\(key) was already set to \(days); not doing anything")
         }
     }
 }
